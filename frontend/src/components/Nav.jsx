c53c43a4a8c3270c97bacc00b1adcbdf7f79df30
Harshal bhawsar <115460223+harshal3007@users.noreply.github.com>
2023-06-12 17:35:42 +0530

    Navbar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React from 'react';
+import logo from '../images/Netflix-Logo-768x432-removebg-preview.png';
+import './Homepage.css';
+import { BiBell } from 'react-icons/bi';
 
-export const nav = () => {
-  
-}
-
+export const Nav = () => {
+  return (
+    <div className="Navbar">
+        <div>
+      <img className="Logo" src={logo} alt="" />
+      <div className="NavItems">
+        <h3>Home</h3>
+        <h3>TV Shows</h3>
+        <h3>Movies</h3>
+        <h3>New & Popular</h3>
+        <h3>My List</h3>
+        <h3>Browse by Languages</h3>
+      </div>
+      </div>
+      <div>
+      <input className="NavInput" type="text" placeholder='Titles,people,generes' />
+      <h3 className="User">User</h3>
+      <h3 className="NotifLogo">
+        <BiBell />
+      </h3>
+      <img className="userImage" src="" alt="userImage" />
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import { Nav } from "../components/Nav"
+
+export const HomePage = () => {
+  return(
+    <>
+      <Nav/>
+    </>
+  )
+}
+
